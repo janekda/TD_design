4c4a1a17a09c7899c6717ddf8973b6bc4e896b5a
unknown <Jan Chodura@.(none)>
2014-09-20 15:51:29 +0200

    zalozeni skriptu na pozadi

---
+++ scripts/background.js
@@ -0,0 +1,12 @@
+
+/**
+ * Vysune se malý frame s alertem o bounties a odkazem na souboj.
+ * Kontrola odměn probíhá co 5 minut. 
+ */
+function showBountiesAlert(){
+   
+setInterval(function () {alert("Hello");}, 3000);
+   
+}
+
+alert("hoj");
